24c74f2e13461e3451562055f5bd7821008e6d14
yohan0321 <144520043+yohan0321@users.noreply.github.com>
2025-04-18 15:16:46 +0900

    Add files via upload

---
+++ hw6.py
@@ -0,0 +1,25 @@
+n = 0
+for i in range(1, 10):
+    for j in range(2, 6):
+        if (n == 4):
+            n = 0
+            print("")
+        print(j, "x", i, "=", i*j, end='\t')
+        n = n + 1
+
+print("")
+print("")
+
+n = 0
+for i in range(1, 10):
+    for j in range(6, 10):
+        if (n == 4):
+            n = 0
+            print("")
+        print(j, "x", i, "=", i*j, end='\t')
+        n = n + 1
+        
+    
+
+
+
